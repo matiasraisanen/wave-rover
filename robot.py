f571f20054781b625c89dc53ebab5d206d3f9194
Matias Räisänen <matias.raisanen@gmail.com>
2023-12-20 16:30:45 +0200

    add robot class

---
+++ robot.py
@@ -0,0 +1,28 @@
+from rover import Rover
+from logger import Logger
+import logging
+from inputDeviceReader import InputDeviceReader
+import threading
+
+
+class Robot:
+    def __init__(self):
+        self.rover = Rover()
+        self.logger = Logger(
+            module_name=__name__,
+            log_file="rover.log",
+            log_level=logging.DEBUG,
+            delete_old_logfile=True,
+        )
+        self.reader = InputDeviceReader()
+
+    def start(self):
+        thread = threading.Thread(
+            target=self.reader.read_events, args=(self.rover.process_input_data,)
+        )
+        thread.start()
+
+
+# Usage
+robot = Robot()
+robot.start()
