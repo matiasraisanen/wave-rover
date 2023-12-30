bccdd1afebbada1b7e765a3f7b9fefa2a2c9e892
Matias Räisänen <matias.raisanen@gmail.com>
2023-12-30 14:54:09 +0200

    use states

--- inputDeviceReader.py
+++ inputDeviceReader.py
@@ -1,8 +1,18 @@
 import evdev
+from logger import Logger
+import logging
 
 
 class InputDeviceReader:
     def __init__(self, device_path=None):
+        self.logger = Logger(
+            module_name=__name__,
+            log_file="rover.log",
+            log_level=logging.DEBUG,
+            delete_old_logfile=True,
+            streamhandler=True,
+            filehandler=True,
+        )
         # If no device path is specified, use the first device in the list
         try:
             if device_path is None:
@@ -12,6 +22,9 @@ class InputDeviceReader:
             raise Exception("No input devices found")
         self.device = evdev.InputDevice(device_path)
 
+        # Initialize key states
+        self.key_states = {2: 0, 5: 0, 0: 0}
+
     def read_events(self, callback):
         for event in self.device.read_loop():
             # EV_KEY is for buttons
@@ -20,14 +33,24 @@ class InputDeviceReader:
                 key_number = key_event.scancode
                 key_name = key_event.keycode
                 key_value = key_event.keystate
-                key_press = callback("key", key_number, key_value, key_name)
+                percentage = None
+                # key_press = callback("key", key_number, key_value)
             # EV_ABS is for analog inputs
             if event.type == evdev.ecodes.EV_ABS:
                 abs_event = evdev.categorize(event)
                 key_number = abs_event.event.code
                 key_value = abs_event.event.value
                 percentage = self.value_to_percentage(key_number, key_value)
-                key_press = callback("abs", key_number, key_value, percentage)
+                # key_press = callback("abs", key_number, key_value, percentage)
+
+            # use key percentage if it is not None, otherwise use key value
+            self.logger.log.debug(
+                f"KeyNumber: {key_number} Value: {key_value} Percentage: {percentage}%"
+            )
+            self.key_states[key_number] = percentage or key_value
+            # self.key_states[key_number] = key_value
+            # callback("abs", key_number, key_value, percentage)
+            callback(self.key_states)
 
     # Returns analog values as percentages
     def value_to_percentage(self, key_number, key_value):
